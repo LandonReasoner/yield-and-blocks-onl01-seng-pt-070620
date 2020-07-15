96925261f9801bcabd736c6bbefcbe2c485d2a88
Landon <reasoner.landon90@gmail.com>
2020-07-15 02:33:00 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(names)
+  if block_given?
   i = 0
 
   while i < names.length
@@ -6,6 +7,8 @@ def hello_t(names)
     i = i + 1
   end
   names
+ else
+   puts "Hey! No block was given!"
 end
 
 # call your method here!
